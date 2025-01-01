a33c8244f4afea8c85dfcf943d738f559c6c1b34
Matteo Melli <matteo.melli@gmail.com>
2025-01-01 17:52:25 +0100

    Day 11 (partial 2 broken)

--- 2024/11/p2/solve.java
+++ 2024/11/p2/solve.java
@@ -5,23 +5,38 @@ import java.util.concurrent.*;
 import java.util.stream.*;
 import java.util.function.*;
 
-public class solve {
+public class resolve {
+
+  static Map<Long, BigInteger> cache = new HasMap<>();
+  static Map<Long, List<Long>> stoneCache = new HasMap<>();
 
   public static void main(String[] args) throws Exception {
     final var input = Files.lines(Paths.get("input"))
       .toList();
-    final var initStones = Arrays.asList(input.getFirst().split(" "));
-    List<List<String>> stones = new ArrayList<>();
+    final var initStones = Arrays.asList(input.getFirst().split(" ")).stream().map(Long::parseLong).toList();
+    List<List<Long>> stones = new ArrayList<>();
     List<Integer> blinks = new ArrayList<>();
     stones.add(initStones);
     blinks.add(Optional.of(args).filter(a -> a.length > 0).map(a -> Integer.parseInt(a[0])).orElse(25));
     final int maxThreads = 1 << 3;
-    final int stoneBatchSize = 1;
+    final int stoneBatchSize = 1 << 16;
+    final int superblinks = 3;
     BigInteger[] count = new BigInteger[] { BigInteger.ZERO };
+    int iterations = 0;
+    for (int superblink = 0; superblink < superblinks; superblink++) {
+    var nextStones = new ArrayList<List<Long>>();
+    for (Long stone : stones.stream().flatMap(List::stream).toList()) {
+    if (cache.containsKey(stone)) {
+      count[0] = count[0].add(cache.get(stone));
+      nextStones.add(stoneCache.get(stone));
+      continue;
+    }
+    stones = List.of(List.of(stone));
     while (!stones.isEmpty()) {
+      iterations++;
       final int threads = Math.min(maxThreads, stones.size());
       //System.out.println("stones: " + IntStream.range(0, threads).mapToObj(thread -> stones.get(thread).size() + " (" + blinks.get(thread) + ")").collect(Collectors.joining(" ")) + " levels " + stones.size() + " " + count[0] + " pending blinks range: " + blinks.stream().mapToInt(b -> b).min().orElse(0) + "-" + blinks.stream().mapToInt(b -> b).max().orElse(0) + " remaining: " + stones.stream().mapToLong(List::size).sum() + " threads: " + threads);
-      final var newStones = IntStream.range(0, threads).mapToObj(thread -> new ArrayList<String>()).toList();
+      final var newStones = IntStream.range(0, threads).mapToObj(thread -> new ArrayList<Long>(stones.get(thread).size() << 1)).toList();
       IntStream.range(0, threads).mapToObj(thread -> CompletableFuture.runAsync(() -> {
         if (stones.get(thread).size() > stoneBatchSize) {
           blink(stones, newStones.get(thread), thread, thread, 0, stoneBatchSize - 1);
@@ -35,11 +50,12 @@ public class solve {
       thread = threads - thread - 1;
       //System.out.println("thread " + thread + " new stones: " + newStones.get(thread).stream().collect(Collectors.joining(" ")) + " (" + newStones.get(thread).size() + ")"); 
       if (stones.get(thread).size() > stoneBatchSize) {
-        stones.add(Math.min(stones.size(), threads), stones.get(thread).subList(stoneBatchSize, stones.get(thread).size()));
-        blinks.add(Math.min(blinks.size(), threads), blinks.get(thread));
+	stones.add(Math.min(stones.size(), threads), stones.get(thread).subList(stoneBatchSize, stones.get(thread).size()));
+	blinks.add(Math.min(blinks.size(), threads), blinks.get(thread));
       }
       if (blinks.get(thread).intValue() == 1) {
         count[0] = count[0].add(BigInteger.valueOf((long) newStones.get(thread).size()));
+	nextStones.add(newStones.get(thread));
         stones.remove(thread);
 	blinks.remove(thread);
       } else {
@@ -47,18 +63,23 @@ public class solve {
         blinks.set(thread, blinks.get(thread) - 1);
       }
       });
-      if (!stones.isEmpty()) {
-      //System.out.println(stones.get(0).size() + " (" + blinks.get(0) + ") levels " + stones.size() + " " + count[0] + " " + blinks.stream().mapToInt(b -> b).min().orElse(0) + "-" + blinks.stream().mapToInt(b -> b).max().orElse(0) + " " + stones.stream().mapToLong(List::size).sum());
+      if (!stones.isEmpty() && iterations % 50 == 0) {
+      System.out.println("[" + iterations + "] " + stones.get(0).size() + " (" + blinks.get(0) + ") levels " + stones.size() + " " + count[0] + " " + blinks.stream().mapToInt(b -> b).min().orElse(0) + "-" + blinks.stream().mapToInt(b -> b).max().orElse(0) + " " + stones.stream().mapToLong(List::size).sum());
       }
     }
+    stones = List.of(nextStones.stream().flatMap(List::stream).toList());
+    }
+    }
+    }
+    System.out.println("iterations: " + iterations);
     System.out.println(count[0]);
   }
 
   static BigInteger N2024 = new BigInteger("2024");
 
   static void blink(
-      List<List<String>> stones,
-      List<String> newStones,
+      List<List<Long>> stones,
+      List<Long> newStones,
       int firstLevelStart,
       int lastLevelEnd,
       int firstIndexStart,
@@ -77,23 +98,25 @@ public class solve {
       endIndex = stones.get(level).size() - 1;
     }
     for (int index = startIndex; index <= endIndex; index++) {
-      String stone = stones.get(level).get(index);
-      if (stone.equals("0")) {
-	newStones.add("1");
+      var stone = stones.get(level).get(index);
+      if (stone.equals(0L)) {
+	newStones.add(1L);
 	continue;
       }
-      if (stone.length() % 2 == 0) {
-	newStones.add(stone.substring(0, stone.length() / 2));
-	var right = stone.substring(stone.length() / 2);
-	int leftZeros = (int) right.chars().takeWhile(c -> c == '0').count();
-	if (leftZeros < right.length()) {
-	  newStones.add(right.substring(leftZeros));
-	} else {
-	  newStones.add("0");
-	}
+      String stoneText = stone.toString();
+      if (stoneText.length() % 2 == 0) {
+	newStones.add(Long.parseLong(stoneText.substring(0, stoneText.length() / 2)));
+	String right = stoneText.substring(stoneText.length() / 2);
+	//int leftZeros = (int) right.chars().takeWhile(c -> c == '0').count();
+	//if (leftZeros < right.length()) {
+	//  newStones.add(right.substring(leftZeros));
+	//} else {
+	//  newStones.add("0");
+	//}
+	newStones.add(Long.parseLong(right));
 	continue;
       }
-      newStones.add(new BigInteger(stone).multiply(N2024).toString());
+      newStones.add(stone * 2024L);
     }
   }
 }
